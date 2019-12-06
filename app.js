a88271bda49cf0452143376cc730215e942bccfb
Tibor Nagy <tibornagy77@gmail.com>
2019-12-06 22:22:30 +0100

    User signup and login implemented

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(process.env.MONGO_ATLAS_CS, {
     useNewUrlParser: true,
@@ -10,6 +13,10 @@ mongoose.connect(process.env.MONGO_ATLAS_CS, {
     useCreateIndex: true,
 }).then(() => {
     console.log('Connected to Mongo Atlas');
+
+    app.use(bodyParser.json());
+
+    app.use("/user", userRoutes);
 });
 
 mongoose.Promise = global.Promise;
